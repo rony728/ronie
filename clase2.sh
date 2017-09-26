0dfdb7470ef3046432d2e85b1b59ccf527fa9903
rony728 <rony728@gmail.com>
2017-09-26 10:55:26 -0600

    codenew

--- clase2.sh
+++ clase2.sh
@@ -1,11 +1,22 @@
-#! bin/sh
+#! /bin/sh
 
-ask () {
-	printf "$@ : "
-	read answer
-	printf "answer \n"
-
+draw () {
+	for a in 6 7 8 9 10 11; do
+		for s in `seq $(( 11 - a ))`; do
+			printf "*"
+		done
+		for c in `seq $(( ))`; do
+			:;
+		done
+		printf "\n"
+	done
 }
 
-ask "sssssssssssyys"
+draw "c"
 
+#     *                      | 5e 1c   6 
+#    ***                     | 4e 3c   7
+#   *****                    | 3e 5c   8
+#  *******                   | 2e 7c   9
+# *********                  | 1e 9c   10
+#***********                 | 0e 11c 11
